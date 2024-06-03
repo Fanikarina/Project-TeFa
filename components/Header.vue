a9795a041fb4205de535fe631ca9fbc36378ef86
Fani Karina <140600965+Fanikarina@users.noreply.github.com>
2024-06-03 09:23:39 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="row p-3 bg-primary text-white d-flex align-items-center">
+    <div class="row p-3 bg-primary text-white d-flex align-items-center sticky-top">
         <div class="col-md-1">
             <img src="~/assets/img/logo-smkn4.png" alt="logo SMKN 4" />     
         </div>
-        <div class="col-md ps-5 text-white spartan" style="">
-            <h1>PERPUSTAKAAN DIGITAL</h1>
+        <div class="col-md text-white spartan" style="">
+            <h2>PERPUSTAKAAN DIGITAL</h2>
         </div>
     </div>
 </template>
@@ -12,7 +12,7 @@
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@700&display=swap');
 img{
-    width: 120px;
+    width: 80px;
 }
 
 @media only screen and (max-width: 600px){
@@ -20,15 +20,15 @@ img{
         text-align: center;
     }
     img{
-        width: 60px;
+        width: 20px;
     }
-    h1{
+    h2{
         font-size: large;
         padding-top: 5%
     }
 }
 @media only screen and (min-width:600px) and (max-width:900px){
-    h1{
+    h2{
         margin-left: 5%;
     }
     
